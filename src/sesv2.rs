1100c3e3e4b13d7cd54466a6ea16d62236b8745b
Anton Sidorov <antosid@gmail.com>
2025-02-15 22:27:15 +0000

    cleanup:

--- src/sesv2.rs
+++ src/sesv2.rs
@@ -15,44 +15,49 @@ pub async fn get_suppression_list(
         .send();
 
     let mut emails = Vec::new();
+    let now = chrono::Utc::now();
 
     while let Some(addresses) = sesv2_addresses_stream.next().await {
         debug!("Addresses: {:?}", addresses);
 
-        for address in addresses.unwrap().suppressed_destination_summaries() {
-            debug!("Address: {:?}", address);
-            let now = chrono::Utc::now();
-            let timestamp = address.last_update_time();
-            match last_count_days {
-                None => {
-                    emails.push((
-                        address.email_address().to_string(),
-                        address.reason().to_string(),
-                        timestamp.to_string(),
-                    ));
-                }
-                Some(last) => {
-                    let time_date = match DateTime::from_timestamp(
-                        timestamp.secs(),
-                        timestamp.subsec_nanos(),
-                    ) {
-                        Some(time_date) => time_date,
-                        None => {
-                            return Err("Error parsing date".into());
-                        }
-                    };
-                    let duration = now - time_date;
-                    info!("Duration: {:?}", duration.num_days());
-                    if duration.num_days() < last as i64 {
-                        emails.push((
+        emails.extend(
+            addresses?
+                .suppressed_destination_summaries()
+                .iter()
+                .filter_map(|address| {
+                    debug!("Address: {:?}", address);
+                    let timestamp = address.last_update_time();
+                    match last_count_days {
+                        None => Some((
                             address.email_address().to_string(),
                             address.reason().to_string(),
                             timestamp.to_string(),
-                        ));
+                        )),
+                        Some(last) => {
+                            let time_date = match DateTime::from_timestamp(
+                                timestamp.secs(),
+                                timestamp.subsec_nanos(),
+                            ) {
+                                Some(time_date) => time_date,
+                                None => {
+                                    return None;
+                                }
+                            };
+                            let duration = now - time_date;
+                            info!("Duration: {:?}", duration.num_days());
+                            if duration.num_days() < last as i64 {
+                                Some((
+                                    address.email_address().to_string(),
+                                    address.reason().to_string(),
+                                    timestamp.to_string(),
+                                ))
+                            } else {
+                                None
+                            }
+                        }
                     }
-                }
-            }
-        }
+                }),
+        );
         thread::sleep(time::Duration::from_millis(1000));
     }
     // Err("Address not found".into())
