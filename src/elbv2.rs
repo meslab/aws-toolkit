9706413af20bce80420a3c3062b5740c53edd191
Anton Sidorov <antosid@gmail.com>
2025-02-15 22:15:58 +0000

    cleanup:

--- src/elbv2.rs
+++ src/elbv2.rs
@@ -6,13 +6,20 @@ pub async fn list_load_balancers(client: &Client, cluster: &str) -> AppResult<Ve
     let mut load_balancers = Vec::new();
     let mut load_balancers_stream = client.describe_load_balancers().into_paginator().send();
 
-    while let Some(load_balancer) = load_balancers_stream.next().await {
-        debug!("Load Balancers: {:?}", load_balancer);
-        for lb in load_balancer.unwrap().load_balancers.unwrap() {
-            if lb.load_balancer_name.unwrap().contains(cluster) {
-                load_balancers.push(lb.load_balancer_arn.unwrap());
-            }
-        }
+    while let Some(load_balancer_output) = load_balancers_stream.next().await {
+        debug!("Load Balancers: {:?}", load_balancer_output);
+        load_balancers.extend(load_balancer_output?.load_balancers().iter().filter_map(
+            |load_balancer| {
+                if !load_balancer
+                    .load_balancer_name
+                    .as_deref()?
+                    .contains(cluster)
+                {
+                    return None;
+                }
+                Some(load_balancer.load_balancer_arn.as_deref()?.to_owned())
+            },
+        ));
     }
     Ok(load_balancers)
 }
